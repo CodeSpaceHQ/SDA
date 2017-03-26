b2dfd920c2ab0a488b0af350a0511bdb31dec623
Kevin Taing <kevin.taing@ttu.edu>
2017-03-26 03:23:51 -0500

    Changed startup script to work with Windows
    -Updated the ReadME to include more requirements
    -Changed the cmd, that is used to start the server, to work with Windows. Users will need to enter activate admin account on their system

--- dbmanager.py
+++ dbmanager.py
@@ -3,8 +3,8 @@ import os
 
 # function to start an instance of the database
 def startUp():
-    os.system("mysql.server start")
+    os.system("runas /user:administrator \"net start mysql57\"")
 
 # function to close an instance of the database
 def stop():
-    os.system("mysql.server stop")
+    os.system("runas /user:administrator \"net stop mysql57\"")
