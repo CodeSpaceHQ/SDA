3e1ea3b2b9d9a6c9021dd88afd7157f23bc41699
dlowrey <dane.r.lowrey@gmail.com>
2017-04-25 11:13:47 -0500

    Added index sql statements and attribute ticks to all attributes

--- initdb.py
+++ initdb.py
@@ -17,7 +17,7 @@ TABLES['income'] = (
     "   `AGI_STUB` tinyint NOT NULL,"
     "   `NUM_RETURNS` float(15,4) NOT NULL,"
     "   `TOTAL_INCOME` float(15,4) NOT NULL,"
-    "   PRIMARY KEY (STATE, ZIPCODE, AGI_STUB)"
+    "   PRIMARY KEY (`STATE`, `ZIPCODE`, `AGI_STUB`)"
     ") ENGINE=InnoDB")
 
 TABLES['starbucks'] = (
@@ -28,7 +28,7 @@ TABLES['starbucks'] = (
     "   `ZIPCODE` char(5) NOT NULL,"
     "   `LONG` varchar(10) NOT NULL,"
     "   `LAT` varchar(10) NOT NULL,"
-    "   PRIMARY KEY (STORE_NUMBER)"
+    "   PRIMARY KEY (`STORE_NUMBER`)"
     ") ENGINE=InnoDB")
 
 TABLES['diversity'] = (
@@ -43,7 +43,7 @@ TABLES['diversity'] = (
     "   `5` float(3,1) NOT NULL,"
     "   `6` float(3,1) NOT NULL,"
     "   `7` float(3,1)NOT NULL,"
-    "   PRIMARY KEY (COUNTY, STATE)"
+    "   PRIMARY KEY (`COUNTY`, `STATE`)"
     ") ENGINE=InnoDB")
 
 TABLES['locations'] = (
@@ -52,8 +52,16 @@ TABLES['locations'] = (
     "   `CITY` varchar(50) NOT NULL,"
     "   `STATE` char(2) NOT NULL,"
     "   `COUNTY` varchar(50) NOT NULL,"
-    "   PRIMARY KEY (ZIPCODE, COUNTY)"
+    "   PRIMARY KEY (`ZIPCODE`, `COUNTY`)"
     ") ENGINE=InnoDB")
+
+
+INDEX = {}
+INDEX['starbucks_zipcode'] = ("CREATE INDEX `starbucks_zipcode` "
+                              "ON `starbucks` (`ZIPCODE`)")
+INDEX['income_zipcode'] = ("CREATE INDEX `income_zipcode` "
+                              "ON `starbucks` (`ZIPCODE`)")
+
 
 # SQL INSERT statements for each table
 SQL_INSERT = {}
@@ -94,9 +102,12 @@ def init_database(connection):
         cursor.execute(create_db)  # Create the database
         connection.database = DB_NAME  # Connect to the database
 
-        for name, query in TABLES.items():  # Create each table in the database
+        for name, sql in TABLES.items():  # Create each table in the database
             print('Creating table {}'.format(name))
-            cursor.execute(query)
+            cursor.execute(sql)
+        for name, sql in TABLES.items():
+            print('Creating index {}'.format(name))  # Create column indexes
+            cursor.execute(sql)
 
         cursor.close()
     except mysql.connector.Error as err:
