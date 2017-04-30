a596816da044e00e99dee997380bc919c07f1c8d
kevintaing86 <kevin.taing@ttu.edu>
2017-04-30 17:37:11 -0500

    Added a choropleth layer

--- testmap/testmap.js
+++ testmap/testmap.js
@@ -8,7 +8,25 @@ function initMap() {
     query: {
       select: '\'Geocodable address\'',
       from: '10eIaqwKkNHAWeRvHMCGk18mTlrypPV7W7FpmVyC-'
+    },
+    heatmap: {
+      enabled: true,
+      valueField: 'AGI_STUB'
     }
+  });
+
+  // testing choropleth layer
+  var choro_layer = new google.maps.Data();
+  choro_layer.loadGeoJson('');
+
+  choro_layer.setStyle(function(feature) {
+    return {
+      fillColor: '',
+      fillOpacity: 0.8,
+      strokeColor: '',
+      strokeWeight: 1,
+      zIndex: 1
+    };
   });
 
   layer.setMap(map);
